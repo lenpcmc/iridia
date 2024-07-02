320e66d111bd38378546d03c17aa3eaf6e012d28
lenpcmc <80722658+lenpcmc@users.noreply.github.com>
2024-07-02 20:18:03 +0000

    PQEq Energy Initial

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pymatgen.io.ase import MSONAtoms
+from ase import Atoms
+from ase.io import read as ase_read
+
+from tqdm import tqdm
+enum = lambda x: tqdm(enumerate(x))
