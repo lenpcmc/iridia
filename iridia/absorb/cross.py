5739357c8128f430d08e9e0dee0df290eb785028
lenpcmc <80722658+lenpcmc@users.noreply.github.com>
2025-01-21 18:00:10 -0600

    Update cross.py

--- iridia/absorb/cross.py
+++ iridia/absorb/cross.py
@@ -6,17 +6,17 @@ c: float = 299_792_458
 
 def absorbance(w: float, k: float, ddm: float, y: float = 0.25) -> float:
     # Init
-    w: np.ndarray = extend(w, 3).swapaxes(0,1)
-    k: np.ndarray = extend(k, 3)
-    ddm: np.ndarray = extend(ddm, 3)
-    y: np.ndarray = extend(y, 3)
+    w: np.ndarray = extend(w, 3)
+    k: np.ndarray = extend(k, 3).swapaxes(0,1)
+    ddm: np.ndarray = extend(ddm, 3).swapaxes(0,1)
+    y: np.ndarray = extend(y, 3).swapaxes(0,1)
 
-    ddm2: np.ndarray = np.sum(ddm**2., axis = 1, keepdims = True)
+    ddm2: np.ndarray = np.sum(ddm**2., axis = 0, keepdims = True)
 
     # Calc
     absCoeff: float = pi / ( 3. * ep0 * c )
     broadening: np.ndarray = y / ((k - w)**2. + y**2.)
 
     #return absCoeff * ddm2 * broadening
-    return np.squeeze(np.sum( absCoeff * ddm2 * broadening, axis = 0 ))
+    return np.squeeze(np.sum(absCoeff * ddm2 * broadening, axis = 1, keepdims = True))
 
